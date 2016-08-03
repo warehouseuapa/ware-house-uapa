5749b9daf50f1a1783e9e89abc3ae7a38e4c6bc2
ware house uapa <warehouseuapa@hotmail.com>
2016-08-03 02:22:52 +0200

    agregar todos los archivos

---
+++ app/src/main/java/warehousemobile/com/Main22Activity.java
@@ -0,0 +1,13 @@
+package warehousemobile.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main22Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main22);
+    }
+}
